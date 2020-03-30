28956e37481310d490e95090d4c69de1252f7f60
Adil Nurtayev <anurtayev@torstar.ca>
2020-03-29 20:33:49 -0400

    stable

--- packages/server/src/generated/graphql.ts
+++ packages/server/src/generated/graphql.ts
@@ -1,6 +1,9 @@
-import { GraphQLResolveInfo } from 'graphql';
+import { GraphQLResolveInfo } from "graphql";
 export type Maybe<T> = T | null;
-export type RequireFields<T, K extends keyof T> = { [X in Exclude<keyof T, K>]?: T[X] } & { [P in K]-?: NonNullable<T[P]> };
+export type RequireFields<T, K extends keyof T> = {
+  [X in Exclude<keyof T, K>]?: T[X];
+} &
+  { [P in K]-?: NonNullable<T[P]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -10,85 +13,79 @@ export type Scalars = {
   Float: number;
 };
 
-export type Entry = {
-  id: Scalars['ID'];
+interface IEntry {
+  id: string;
   metaData?: Maybe<MetaData>;
-  type: Scalars['String'];
-};
+  type: Scalars["String"];
+}
 
-export type File = Entry & {
-   __typename?: 'File';
-  id: Scalars['ID'];
-  metaData?: Maybe<MetaData>;
-  type: Scalars['String'];
-  size: Scalars['Int'];
-  thumbImage?: Maybe<Scalars['String']>;
-  contentType?: Maybe<Scalars['String']>;
-};
+export type Entry = File | Folder;
 
-export type Folder = Entry & {
-   __typename?: 'Folder';
-  id: Scalars['ID'];
+export interface File extends IEntry {
+  __typename?: "File";
+  id: Scalars["ID"];
   metaData?: Maybe<MetaData>;
-  type: Scalars['String'];
+  type: "File";
+  size: Scalars["Int"];
+  thumbImage?: Maybe<Scalars["String"]>;
+  contentType: string;
+}
+
+export interface Folder extends IEntry {
+  __typename?: "Folder";
+  id: Scalars["ID"];
+  metaData?: Maybe<MetaData>;
+  type: "Folder";
   children?: Maybe<Array<Entry>>;
-};
+}
 
 export type MetaData = {
-   __typename?: 'MetaData';
-  tags?: Maybe<Array<Scalars['String']>>;
-  attributes?: Maybe<Array<Array<Scalars['String']>>>;
-  title?: Maybe<Scalars['String']>;
-  description?: Maybe<Scalars['String']>;
+  __typename?: "MetaData";
+  tags?: Maybe<Array<Scalars["String"]>>;
+  attributes?: Maybe<Array<Array<Scalars["String"]>>>;
+  title?: Maybe<Scalars["String"]>;
+  description?: Maybe<Scalars["String"]>;
 };
 
 export type Mutation = {
-   __typename?: 'Mutation';
+  __typename?: "Mutation";
   addTag?: Maybe<MetaData>;
   removeTag?: Maybe<MetaData>;
   addAttribute?: Maybe<MetaData>;
   removeAttribute?: Maybe<MetaData>;
 };
 
-
 export type MutationAddTagArgs = {
-  id: Scalars['String'];
-  tag: Scalars['String'];
+  id: Scalars["String"];
+  tag: Scalars["String"];
 };
-
 
 export type MutationRemoveTagArgs = {
-  id: Scalars['String'];
-  tag: Scalars['String'];
+  id: Scalars["String"];
+  tag: Scalars["String"];
 };
-
 
 export type MutationAddAttributeArgs = {
-  id: Scalars['String'];
-  attribute: Array<Scalars['String']>;
+  id: Scalars["String"];
+  attribute: Array<Scalars["String"]>;
 };
 
-
 export type MutationRemoveAttributeArgs = {
-  id: Scalars['String'];
-  attributeKey: Scalars['String'];
+  id: Scalars["String"];
+  attributeKey: Scalars["String"];
 };
 
 export type Query = {
-   __typename?: 'Query';
+  __typename?: "Query";
   getRootFolderEntries?: Maybe<Array<Entry>>;
   getFolderEntries?: Maybe<Array<Entry>>;
 };
 
-
 export type QueryGetFolderEntriesArgs = {
-  id: Scalars['String'];
+  id: Scalars["String"];
 };
 
-
-
 export type ResolverTypeWrapper<T> = Promise<T> | T;
-
 
 export type StitchingResolver<TResult, TParent, TContext, TArgs> = {
   fragment: string;
@@ -120,9 +117,25 @@ export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
   info: GraphQLResolveInfo
 ) => TResult | Promise<TResult>;
 
-export interface SubscriptionSubscriberObject<TResult, TKey extends string, TParent, TContext, TArgs> {
-  subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>;
-  resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>;
+export interface SubscriptionSubscriberObject<
+  TResult,
+  TKey extends string,
+  TParent,
+  TContext,
+  TArgs
+> {
+  subscribe: SubscriptionSubscribeFn<
+    { [key in TKey]: TResult },
+    TParent,
+    TContext,
+    TArgs
+  >;
+  resolve?: SubscriptionResolveFn<
+    TResult,
+    { [key in TKey]: TResult },
+    TContext,
+    TArgs
+  >;
 }
 
 export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
@@ -130,12 +143,26 @@ export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
   resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
 }
 
-export type SubscriptionObject<TResult, TKey extends string, TParent, TContext, TArgs> =
+export type SubscriptionObject<
+  TResult,
+  TKey extends string,
+  TParent,
+  TContext,
+  TArgs
+> =
   | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
   | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
 
-export type SubscriptionResolver<TResult, TKey extends string, TParent = {}, TContext = {}, TArgs = {}> =
-  | ((...args: any[]) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
+export type SubscriptionResolver<
+  TResult,
+  TKey extends string,
+  TParent = {},
+  TContext = {},
+  TArgs = {}
+> =
+  | ((
+      ...args: any[]
+    ) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
   | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
 
 export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
@@ -144,11 +171,19 @@ export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
   info: GraphQLResolveInfo
 ) => Maybe<TTypes> | Promise<Maybe<TTypes>>;
 
-export type isTypeOfResolverFn<T = {}> = (obj: T, info: GraphQLResolveInfo) => boolean | Promise<boolean>;
+export type isTypeOfResolverFn<T = {}> = (
+  obj: T,
+  info: GraphQLResolveInfo
+) => boolean | Promise<boolean>;
 
 export type NextResolverFn<T> = () => Promise<T>;
 
-export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs = {}> = (
+export type DirectiveResolverFn<
+  TResult = {},
+  TParent = {},
+  TContext = {},
+  TArgs = {}
+> = (
   next: NextResolverFn<TResult>,
   parent: TParent,
   args: TArgs,
@@ -158,89 +193,171 @@ export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs
 
 /** Mapping between all available schema types and the resolvers types */
 export type ResolversTypes = {
-  Query: ResolverTypeWrapper<{}>,
-  Entry: ResolversTypes['Folder'] | ResolversTypes['File'],
-  ID: ResolverTypeWrapper<Scalars['ID']>,
-  MetaData: ResolverTypeWrapper<MetaData>,
-  String: ResolverTypeWrapper<Scalars['String']>,
-  Mutation: ResolverTypeWrapper<{}>,
-  Boolean: ResolverTypeWrapper<Scalars['Boolean']>,
-  Folder: ResolverTypeWrapper<Folder>,
-  File: ResolverTypeWrapper<File>,
-  Int: ResolverTypeWrapper<Scalars['Int']>,
+  Query: ResolverTypeWrapper<{}>;
+  Entry: ResolversTypes["Folder"] | ResolversTypes["File"];
+  ID: ResolverTypeWrapper<Scalars["ID"]>;
+  MetaData: ResolverTypeWrapper<MetaData>;
+  String: ResolverTypeWrapper<Scalars["String"]>;
+  Mutation: ResolverTypeWrapper<{}>;
+  Boolean: ResolverTypeWrapper<Scalars["Boolean"]>;
+  Folder: ResolverTypeWrapper<Folder>;
+  File: ResolverTypeWrapper<File>;
+  Int: ResolverTypeWrapper<Scalars["Int"]>;
 };
 
 /** Mapping between all available schema types and the resolvers parents */
 export type ResolversParentTypes = {
-  Query: {},
-  Entry: ResolversParentTypes['Folder'] | ResolversParentTypes['File'],
-  ID: Scalars['ID'],
-  MetaData: MetaData,
-  String: Scalars['String'],
-  Mutation: {},
-  Boolean: Scalars['Boolean'],
-  Folder: Folder,
-  File: File,
-  Int: Scalars['Int'],
+  Query: {};
+  Entry: ResolversParentTypes["Folder"] | ResolversParentTypes["File"];
+  ID: Scalars["ID"];
+  MetaData: MetaData;
+  String: Scalars["String"];
+  Mutation: {};
+  Boolean: Scalars["Boolean"];
+  Folder: Folder;
+  File: File;
+  Int: Scalars["Int"];
 };
 
-export type EntryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Entry'] = ResolversParentTypes['Entry']> = {
-  __resolveType: TypeResolveFn<'Folder' | 'File', ParentType, ContextType>,
-  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>,
-  metaData?: Resolver<Maybe<ResolversTypes['MetaData']>, ParentType, ContextType>,
-  type?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
+export type EntryResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes["Entry"] = ResolversParentTypes["Entry"]
+> = {
+  __resolveType: TypeResolveFn<"Folder" | "File", ParentType, ContextType>;
+  id?: Resolver<ResolversTypes["ID"], ParentType, ContextType>;
+  metaData?: Resolver<
+    Maybe<ResolversTypes["MetaData"]>,
+    ParentType,
+    ContextType
+  >;
+  type?: Resolver<ResolversTypes["String"], ParentType, ContextType>;
 };
 
-export type FileResolvers<ContextType = any, ParentType extends ResolversParentTypes['File'] = ResolversParentTypes['File']> = {
-  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>,
-  metaData?: Resolver<Maybe<ResolversTypes['MetaData']>, ParentType, ContextType>,
-  type?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
-  size?: Resolver<ResolversTypes['Int'], ParentType, ContextType>,
-  thumbImage?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>,
-  contentType?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>,
-  __isTypeOf?: isTypeOfResolverFn<ParentType>,
+export type FileResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes["File"] = ResolversParentTypes["File"]
+> = {
+  id?: Resolver<ResolversTypes["ID"], ParentType, ContextType>;
+  metaData?: Resolver<
+    Maybe<ResolversTypes["MetaData"]>,
+    ParentType,
+    ContextType
+  >;
+  type?: Resolver<ResolversTypes["String"], ParentType, ContextType>;
+  size?: Resolver<ResolversTypes["Int"], ParentType, ContextType>;
+  thumbImage?: Resolver<
+    Maybe<ResolversTypes["String"]>,
+    ParentType,
+    ContextType
+  >;
+  contentType?: Resolver<
+    Maybe<ResolversTypes["String"]>,
+    ParentType,
+    ContextType
+  >;
+  __isTypeOf?: isTypeOfResolverFn<ParentType>;
 };
 
-export type FolderResolvers<ContextType = any, ParentType extends ResolversParentTypes['Folder'] = ResolversParentTypes['Folder']> = {
-  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>,
-  metaData?: Resolver<Maybe<ResolversTypes['MetaData']>, ParentType, ContextType>,
-  type?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
-  children?: Resolver<Maybe<Array<ResolversTypes['Entry']>>, ParentType, ContextType>,
-  __isTypeOf?: isTypeOfResolverFn<ParentType>,
+export type FolderResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes["Folder"] = ResolversParentTypes["Folder"]
+> = {
+  id?: Resolver<ResolversTypes["ID"], ParentType, ContextType>;
+  metaData?: Resolver<
+    Maybe<ResolversTypes["MetaData"]>,
+    ParentType,
+    ContextType
+  >;
+  type?: Resolver<ResolversTypes["String"], ParentType, ContextType>;
+  children?: Resolver<
+    Maybe<Array<ResolversTypes["Entry"]>>,
+    ParentType,
+    ContextType
+  >;
+  __isTypeOf?: isTypeOfResolverFn<ParentType>;
 };
 
-export type MetaDataResolvers<ContextType = any, ParentType extends ResolversParentTypes['MetaData'] = ResolversParentTypes['MetaData']> = {
-  tags?: Resolver<Maybe<Array<ResolversTypes['String']>>, ParentType, ContextType>,
-  attributes?: Resolver<Maybe<Array<Array<ResolversTypes['String']>>>, ParentType, ContextType>,
-  title?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>,
-  description?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>,
-  __isTypeOf?: isTypeOfResolverFn<ParentType>,
+export type MetaDataResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes["MetaData"] = ResolversParentTypes["MetaData"]
+> = {
+  tags?: Resolver<
+    Maybe<Array<ResolversTypes["String"]>>,
+    ParentType,
+    ContextType
+  >;
+  attributes?: Resolver<
+    Maybe<Array<Array<ResolversTypes["String"]>>>,
+    ParentType,
+    ContextType
+  >;
+  title?: Resolver<Maybe<ResolversTypes["String"]>, ParentType, ContextType>;
+  description?: Resolver<
+    Maybe<ResolversTypes["String"]>,
+    ParentType,
+    ContextType
+  >;
+  __isTypeOf?: isTypeOfResolverFn<ParentType>;
 };
 
-export type MutationResolvers<ContextType = any, ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']> = {
-  addTag?: Resolver<Maybe<ResolversTypes['MetaData']>, ParentType, ContextType, RequireFields<MutationAddTagArgs, 'id' | 'tag'>>,
-  removeTag?: Resolver<Maybe<ResolversTypes['MetaData']>, ParentType, ContextType, RequireFields<MutationRemoveTagArgs, 'id' | 'tag'>>,
-  addAttribute?: Resolver<Maybe<ResolversTypes['MetaData']>, ParentType, ContextType, RequireFields<MutationAddAttributeArgs, 'id' | 'attribute'>>,
-  removeAttribute?: Resolver<Maybe<ResolversTypes['MetaData']>, ParentType, ContextType, RequireFields<MutationRemoveAttributeArgs, 'id' | 'attributeKey'>>,
+export type MutationResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes["Mutation"] = ResolversParentTypes["Mutation"]
+> = {
+  addTag?: Resolver<
+    Maybe<ResolversTypes["MetaData"]>,
+    ParentType,
+    ContextType,
+    RequireFields<MutationAddTagArgs, "id" | "tag">
+  >;
+  removeTag?: Resolver<
+    Maybe<ResolversTypes["MetaData"]>,
+    ParentType,
+    ContextType,
+    RequireFields<MutationRemoveTagArgs, "id" | "tag">
+  >;
+  addAttribute?: Resolver<
+    Maybe<ResolversTypes["MetaData"]>,
+    ParentType,
+    ContextType,
+    RequireFields<MutationAddAttributeArgs, "id" | "attribute">
+  >;
+  removeAttribute?: Resolver<
+    Maybe<ResolversTypes["MetaData"]>,
+    ParentType,
+    ContextType,
+    RequireFields<MutationRemoveAttributeArgs, "id" | "attributeKey">
+  >;
 };
 
-export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = {
-  getRootFolderEntries?: Resolver<Maybe<Array<ResolversTypes['Entry']>>, ParentType, ContextType>,
-  getFolderEntries?: Resolver<Maybe<Array<ResolversTypes['Entry']>>, ParentType, ContextType, RequireFields<QueryGetFolderEntriesArgs, 'id'>>,
+export type QueryResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes["Query"] = ResolversParentTypes["Query"]
+> = {
+  getRootFolderEntries?: Resolver<
+    Maybe<Array<ResolversTypes["Entry"]>>,
+    ParentType,
+    ContextType
+  >;
+  getFolderEntries?: Resolver<
+    Maybe<Array<ResolversTypes["Entry"]>>,
+    ParentType,
+    ContextType,
+    RequireFields<QueryGetFolderEntriesArgs, "id">
+  >;
 };
 
 export type Resolvers<ContextType = any> = {
-  Entry?: EntryResolvers,
-  File?: FileResolvers<ContextType>,
-  Folder?: FolderResolvers<ContextType>,
-  MetaData?: MetaDataResolvers<ContextType>,
-  Mutation?: MutationResolvers<ContextType>,
-  Query?: QueryResolvers<ContextType>,
+  Entry?: EntryResolvers;
+  File?: FileResolvers<ContextType>;
+  Folder?: FolderResolvers<ContextType>;
+  MetaData?: MetaDataResolvers<ContextType>;
+  Mutation?: MutationResolvers<ContextType>;
+  Query?: QueryResolvers<ContextType>;
 };
-
 
 /**
  * @deprecated
  * Use "Resolvers" root object instead. If you wish to get "IResolvers", add "typesPrefix: I" to your config.
-*/
+ */
 export type IResolvers<ContextType = any> = Resolvers<ContextType>;
